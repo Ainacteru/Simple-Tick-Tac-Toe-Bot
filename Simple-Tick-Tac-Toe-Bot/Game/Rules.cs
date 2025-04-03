af16b3c28fc4166760a7e587fd5bf6f2b91b73f6
Ainacteru <162664298+Ainacteru@users.noreply.github.com>
2025-04-02 21:51:19 -0700

    winning horizontally

--- Simple-Tick-Tac-Toe-Bot/Game/Rules.cs
+++ Simple-Tick-Tac-Toe-Bot/Game/Rules.cs
@@ -1,44 +1,69 @@
 public class Rules {
-    private string winner = "";
 
     private Board board;
+    private bool? winner = null; //null = no one wins, true = x wins, false = o wins || basically using a nullable boolean as a tri-state boolean
 
     public Rules(Board board) {
         this.board = board;
     }
 
     public void win() {
+        TestHorizontalSquares();
+    }
+
+    private void TestHorizontalSquares() {
         int numberToTest = 3;
 
         string[] squares = board.GetSquares();
 
-        for (int i = 0; i < 9; i++) //testing 
+        for (int i = 0; i < 9; i++)
         {
             int num = 0;
-            foreach (var values in squares.Take(numberToTest).ToArray()) {
+            foreach (var values in squares.Take(numberToTest).ToArray()) { //tests if someone wins horizontally
                 
                 if (values == "X") {
                     string value = values.ToString();
                     num ++;
                     if (num == 3) {
-                        winner = value;
+                        winner = true;
+                        return;
                     }
                 }
                 else if (values == "O") {
                     string value = values.ToString();
                     num ++;
                     if (num == 3) {
-                        winner = value;
+                        winner = false;
+                        return;
                     }
                 }
             }
 
             numberToTest += 3;
         }
+        winner = null;
+        return;
+    }
+    private void TestVerticalSquares() {
+        //vertical squares are all a difference/sum of 3
+        for (int i = 0; i < 9; i++) {
+            string[] squares = board.GetSquares();
+
+            foreach (var values in squares) {
+                
+            }
+        }
+
     }
 
     public string GetWinner() {
-        return winner + " wins!";
+        if (winner == true) {
+            return "X won!";
+        } else if (winner == false) {
+            return "O won!";
+        } else {
+            return "No one won";
+        }
     }
 
 }
